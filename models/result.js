987cde875c21562718284d95a4e5c7df643aef41
sumitkumar4183 <sk8292414183@gmail.com>
2024-09-25 21:21:59 +0530

    modified:   models/result.js

--- models/result.js
+++ models/result.js
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 
 const ResultSchema  = new mongoose.Schema({
     name: {type:String,required:true},
-    fatherName:{type:String},
+    fatherName:{type:String,required:true},
+    motherName:{type:String,required:true},
     schoolName:{type:String,required:true},
     address:{type:String,required:true},
-    math:{type:Number,required:true},
-    science:{type:Number,required:true},
-    hindi:{type:Number,required:true},
-    english:{type:Number,required:true},
-    socialScience:{type:Number,required:true},
+    maths:{type:String,required:true},
+    science:{type:String,required:true},
+    hindi:{type:String,required:true},
+    english:{type:String,required:true},
+    socialScience:{type:String,required:true},
 })
 
 export default mongoose.models.Result || mongoose.model("Result",ResultSchema)
