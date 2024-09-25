42d2c42703d50e0d2ad9302778b2fae51edebb45
theaman82 <amankumaraman55555@gmail.com>
2024-09-25 11:58:39 +0530

    Db name updated

--- models/result.js
+++ models/result.js
@@ -4,10 +4,15 @@ import mongoose from "mongoose"
 
 
 const ResultSchema  = new mongoose.Schema({
-    name: {type:String},
+    name: {type:String,required:true},
     fatherName:{type:String},
-    schoolName:{type:String},
-    adderss:{type:String}
+    schoolName:{type:String,required:true},
+    address:{type:String,required:true},
+    math:{type:Number,required:true},
+    science:{type:Number,required:true},
+    hindi:{type:Number,required:true},
+    english:{type:Number,required:true},
+    socialScience:{type:Number,required:true},
 })
 
-export default mongoose.models.result || mongoose.model("ResultSchema",ResultSchema)
+export default mongoose.models.Result || mongoose.model("Result",ResultSchema)
