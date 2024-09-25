d1d47b246c9bf0cfd4e167706d40678d6f9ae3eb
sumitkumar4183 <sk8292414183@gmail.com>
2024-09-25 21:17:59 +0530

    modified:   app/component/Insert.jsx

--- app/component/Insert.jsx
+++ app/component/Insert.jsx
@@ -1,8 +1,34 @@
+import result from '@/models/result';
 import React from 'react'
 
 const Insert = () => {
+
+  // server action
+  const handleInsert = async (formData) => {
+    "use server";
+    let name = formData.get("name");
+    let fatherName = formData.get("fatherName");
+    let motherName = formData.get("motherName");
+    let schoolName = formData.get("schoolName");
+    let address = formData.get("address");
+    let english = formData.get("english");
+    let hindi = formData.get("hindi");
+    let maths = formData.get("maths");
+    let science = formData.get("science");
+    let socialScience = formData.get("socialScience");
+
+    DbConnect();
+
+    let newdata = await result.create({name:name,fatherName:fatherName,motherName:motherName,schoolName:schoolName,
+      address:address,english:english,hindi:hindi,maths:maths,science:science,socialScience:socialScience,
+    })
+
+    redirect("/");
+}
+
+
   return (
-    <form action="" className='flex flex-1 px-5 py-6 bg-white font-serif'>
+    <form action={handleInsert} method='post' className='flex flex-1 px-5 py-6 bg-white font-serif'>
       <div className='flex flex-col gap-7'>
         <h1 className='flex font-bold text-indigo-600 text-3xl justify-center'>Student's Details</h1>
         <div className='flex gap-4 justify-evenly'>
@@ -24,7 +50,7 @@ const Insert = () => {
 
           <div className='flex flex-col'>
             <label htmlFor="schoolName" className='font-bold'>School's Name</label>
-            <input type="text" id='fatherName' placeholder='Enter Your SchoolName' className='h-10 placeholder:text-sm outline-none border-2 border-gray-300 px-1 rounded-md' />
+            <input type="text" id='schoolName' placeholder='Enter Your SchoolName' className='h-10 placeholder:text-sm outline-none border-2 border-gray-300 px-1 rounded-md' />
           </div>
         </div>
 
