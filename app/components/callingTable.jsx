dd638cd567194964b5ffa41fa4095eda5e33d448
sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2024-09-25 12:15:57 +0530

    callingTable

---
+++ app/components/callingTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const CallingTable = () => {
+  return (
+     <div className='flex  gap-2 '>
+
+     <table className='border w-full'>
+        <thead>
+          <tr>
+          <th className='border p-3'>Name</th>
+          <th className='border p-3'> Father's name</th>
+          <th className='border p-3'> School Name</th>
+          <th className='border p-3'> address</th>
+          <th className='border p-3'>Maths</th>
+          <th className='border p-3'>Science</th>
+          <th className='border p-3'>SST</th>
+          <th className='border p-3'>Hindi</th>
+          <th className='border p-3'>English</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+
+     </table>
+    
+    </div>
+
+  )
+}
+
+export default CallingTable
